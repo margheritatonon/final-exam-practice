08e75ef9cb48ff958a04409e19801800cc17cf8a
Margherita <mtonon.ieu2023@student.ie.edu>
2024-05-08 15:39:54 +0200

    Added the function lists_division.

---
+++ practice.py
@@ -0,0 +1,11 @@
+def lists_division(x, y):
+    sum_x = sum(x)
+    sum_y = sum(y)
+    
+    return sum_y / sum_x
+
+# Example usage:
+x_values = [10, 20, 30]
+y_values = [5, 10, 15]
+result = lists_division(x_values, y_values)
+print("Result:", result)
